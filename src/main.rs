2bec5336c6a9bc63d2b2a28b6e7c3dfa8ec3d822
Alexander Egorov <egoroff@gmail.com>
2023-06-24 22:36:21 +0300

    refactor: one more unwrap eliminated

--- src/main.rs
+++ src/main.rs
@@ -237,8 +237,12 @@ fn show_extensions(root: &str, output_as_html: bool, cmd: &ArgMatches) -> std::i
         .filter(|(e, _c)| e.len() <= max_ext_len)
         .sorted_by(|a, b| Ord::cmp(&b.1, &a.1))
         .enumerate()
-        .take_while(|(num, (_, _))| {
-            show_top_extensions.is_none() || *show_top_extensions.unwrap() > *num
+        .take_while(|(count, (_, _))| {
+            if let Some(limit) = show_top_extensions {
+                *limit > *count
+            } else {
+                true
+            }
         })
         .for_each(|(num, (ext, count))| {
             resulter.append_count_row(ext, num + 1, *count);
